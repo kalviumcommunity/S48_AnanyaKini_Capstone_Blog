223c0772a3a93ed018918f01b681751d92cecadd
Ananya1812 <ananyakini04@gmail.com>
2024-04-05 09:47:09 +0530

    added gitignore file

---
+++ Backend/Index.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const cors = require("cors");
+require(dotenv).config();
+
+const app = express();
+app.listen(5000, () => console.log("Server is running on port 5000"));
