2c2ffb3191dc28e6a7aef60aa5b255b586c55925
Ananya Kini .M <141708282+Ananya1812@users.noreply.github.com>
2024-04-05 09:51:49 +0530

    Delete Backend directory

--- Backend/Index.js
+++
@@ -1,6 +0,0 @@
-const express = require("express");
-const cors = require("cors");
-require(dotenv).config();
-
-const app = express();
-app.listen(5000, () => console.log("Server is running on port 5000"));
