e7f91a82e7a53fa3e0ab05b9b7c77f79dadddea5
Ananya1812 <ananyakini04@gmail.com>
2024-04-06 11:23:52 +0530

    added necessary routes into userRoutes.js file

--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -1,14 +1,14 @@
 const { Router } = require("express");
 const router = Router();
-const {
-  registerUser,
-  loginUser,
-  getAuthors,
-  getUser,
-  changeAvatar,
-  editUser,
-} = require("../controllers/userControllers");
-router.get("/", (req, res, next) => {
-  res.json("This is the user route");
-});
+
+const {registerUser,loginUser,getAuthors,getUser,changeAvatar,editUser} = require("../controllers/userControllers")
+
+router.post("/register", registerUser)
+router.post("/login", loginUser)
+router.post("/:id", getUser)
+router.post("/change-avatar", changeAvatar)
+router.post("/edit-user", editUser)
+router.post("/", getAuthors)
+
+
 module.exports = router;
